7da0cb0ca34dd1b7e16720591d60b68124043e22
Ryan Sobol <hello@ryansobol.com>
2019-12-02 09:52:43 -0800

    refactor

--- Day2.playground/Contents.swift
+++ Day2.playground/Contents.swift
@@ -12,22 +12,31 @@ intcode[2] = 2
 
 func runProgram(_ intcode: [Int]) -> Int {
   var memory = intcode
-  var optcodePtr = 0
-  var optcode = memory[optcodePtr]
 
-  while optcode != 99 {
+  for optcodePtr in stride(from: 0, to: memory.count - 1, by: 4) {
     let leftPtr = memory[optcodePtr + 1]
     let rightPtr = memory[optcodePtr + 2]
     let resultPtr = memory[optcodePtr + 3]
 
+    let optcode = memory[optcodePtr]
     let left = memory[leftPtr]
     let right = memory[rightPtr]
-    let result = optcode == 1 ? left + right : left * right
+    let operation: (Int, Int) -> Int
 
-    memory[resultPtr] = result
+    if optcode == 1 {
+      operation = (+)
+    }
+    else if optcode == 2 {
+      operation = (*)
+    }
+    else if optcode == 99 {
+      break
+    }
+    else {
+      preconditionFailure("Invalid optcode: \(optcode)")
+    }
 
-    optcodePtr += 4
-    optcode = memory[optcodePtr]
+    memory[resultPtr] = operation(left, right)
   }
 
   return memory[0]
@@ -35,21 +44,23 @@ func runProgram(_ intcode: [Int]) -> Int {
 
 print(runProgram(intcode))
 
-func searchInputs(_ target: Int) -> Int {
-  for noun in 0...99 {
-    for verb in 0...99 {
-      intcode[1] = noun
-      intcode[2] = verb
-
-      let result = runProgram(intcode)
-
-      if result == target {
-        return 100 * noun + verb
-      }
+func checkForMatch(_ target: Int, _ memory: [Int], _ result: Int) {
+  DispatchQueue.global().async {
+    if runProgram(memory) == target {
+      print(result)
     }
   }
-
-  return -1
 }
 
-print(searchInputs(19690720))
+let target = 19690720
+
+for noun in 0...99 {
+  for verb in 0...99 {
+    intcode[1] = noun
+    intcode[2] = verb
+
+    let result = 100 * noun + verb
+
+    checkForMatch(target, intcode, result)
+  }
+}
