a1a135419dc727e55401b6d511a3bfde63753a8b
Ryan Sobol <hello@ryansobol.com>
2019-12-02 07:50:05 -0800

    Add solution 2 for day 2

--- Day2.playground/Contents.swift
+++ Day2.playground/Contents.swift
@@ -1,36 +1,55 @@
 import Foundation
 
 let url = Bundle.main.url(forResource: "input", withExtension: "txt")
-let contents = try! String(contentsOf: url!)
-var input = contents
+let input = try! String(contentsOf: url!)
+var intcode = input
   .trimmingCharacters(in: .newlines)
   .split(separator: ",")
   .map { Int($0)! }
 
-input[1] = 12
-input[2] = 2
+intcode[1] = 12
+intcode[2] = 2
 
-func runProgram(_ input: [Int]) -> Int {
-  var integers = input
-  var optcodeIdx = 0
-  var optcode = integers[optcodeIdx]
+func runProgram(_ intcode: [Int]) -> Int {
+  var memory = intcode
+  var optcodePtr = 0
+  var optcode = memory[optcodePtr]
 
   while optcode != 99 {
-    let leftIdx = integers[optcodeIdx + 1]
-    let rightIdx = integers[optcodeIdx + 2]
-    let resultIdx = integers[optcodeIdx + 3]
+    let leftPtr = memory[optcodePtr + 1]
+    let rightPtr = memory[optcodePtr + 2]
+    let resultPtr = memory[optcodePtr + 3]
 
-    let left = integers[leftIdx]
-    let right = integers[rightIdx]
+    let left = memory[leftPtr]
+    let right = memory[rightPtr]
     let result = optcode == 1 ? left + right : left * right
 
-    integers[resultIdx] = result
+    memory[resultPtr] = result
 
-    optcodeIdx += 4
-    optcode = integers[optcodeIdx]
+    optcodePtr += 4
+    optcode = memory[optcodePtr]
   }
 
-  return integers[0]
+  return memory[0]
 }
 
-print(runProgram(input))
+print(runProgram(intcode))
+
+func searchInputs(_ target: Int) -> Int {
+  for noun in 0...99 {
+    for verb in 0...99 {
+      intcode[1] = noun
+      intcode[2] = verb
+
+      let result = runProgram(intcode)
+
+      if result == target {
+        return 100 * noun + verb
+      }
+    }
+  }
+
+  return -1
+}
+
+print(searchInputs(19690720))
