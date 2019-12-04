9f6aaf3bb76f0bdb7485633d45ed861ea9541ed8
Ryan Sobol <hello@ryansobol.com>
2019-12-04 15:04:55 -0800

    refactor

--- Day4.playground/Contents.swift
+++ Day4.playground/Contents.swift
@@ -5,20 +5,23 @@ func transformPasswordToDigits(password: Int) -> [Int] {
 func hasIncreasingDigits(digits: [Int]) -> Bool {
   let lastIndex = digits.endIndex - 1
 
-  return (0..<lastIndex).filter { index in digits[index] <= digits[index + 1] }.count == lastIndex
+  return (0..<lastIndex)
+    .filter { index in digits[index] <= digits[index + 1] }
+    .count == lastIndex
 }
 
 func hasMatchingTwoDigits(digits: [Int]) -> Bool {
   let lastIndex = digits.endIndex - 1
 
-  return (0..<lastIndex).filter { index in digits[index] == digits[index + 1] }.count > 0
+  return (0..<lastIndex)
+    .filter { index in digits[index] == digits[index + 1] }
+    .count > 0
 }
 
 func hasGroupingOfTwoDigits(digits: [Int]) -> Bool {
-  return digits.reduce(into: [:]) { accum, digit in
-    accum[digit, default: 0] += 1
-  }
-  .values.contains(2)
+  return digits
+    .reduce(into: [:]) { accum, digit in accum[digit, default: 0] += 1 }
+    .values.contains(2)
 }
 
 let passwordRange = 234208...765869
