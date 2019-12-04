88f981cc4a4525cfbb15b1805e097b476d7c0738
Ryan Sobol <hello@ryansobol.com>
2019-12-04 14:52:38 -0800

    refactor

--- Day4.playground/Contents.swift
+++ Day4.playground/Contents.swift
@@ -1,18 +1,36 @@
+func transformPasswordToDigits(password: Int) -> [Int] {
+  return String(password).compactMap { $0.wholeNumberValue }
+}
+
+func hasIncreasingDigits(digits: [Int]) -> Bool {
+  return (0...4).filter { digits[$0] <= digits[$0 + 1] }.count == 5
+}
+
+func hasMatchingTwoDigits(digits: [Int]) -> Bool {
+  return (0...4).filter { digits[$0] == digits[$0 + 1] }.count > 0
+}
+
+func hasGroupingOfTwoDigits(digits: [Int]) -> Bool {
+  return digits.reduce(into: [:]) { accum, digit in
+    accum[digit, default: 0] += 1
+  }.values.contains(2)
+}
+
 let passwordRange = 234208...765869
 
 let increasingDigits = passwordRange
   .lazy
-  .map { (password: Int) in String(password).compactMap { $0.wholeNumberValue } }
-  .filter { (digits: [Int]) in (0...4).filter { digits[$0] <= digits[$0 + 1] }.count == 5 }
+  .map(transformPasswordToDigits)
+  .filter(hasIncreasingDigits)
 
 let answer1 = increasingDigits
-  .filter { (digits: [Int]) in (0...4).filter { digits[$0] == digits[$0 + 1] }.count > 0 }
+  .filter(hasMatchingTwoDigits)
   .count
 
 print(answer1)
 
 let answer2 = increasingDigits
-  .filter { (digits: [Int]) in digits.reduce(into: [:]) { $0[$1, default: 0] += 1 }.values.contains(2) }
+  .filter(hasGroupingOfTwoDigits)
   .count
 
 print(answer2)
